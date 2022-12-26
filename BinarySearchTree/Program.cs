ce7c3f4c59c3a5a7b61d09a15bcdb24f6c1f6b49
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2022-12-26 17:20:07 +0700

    class node

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -4,10 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BinarySearchTree
+namespace BinarySearchTree136
 {
+    class node
+    {
+        public string info;
+        public node lchild;
+        public node rchild;
+
+        public node(string i, node l, node r)
+        {
+            info = i;
+            lchild = l;
+            rchild = r;
+        }
+    }
     class Program
     {
+        // A node Class const
+
         static void Main(string[] args)
         {
         }
